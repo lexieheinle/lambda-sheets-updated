e0bc2366e2bf37df3c2d15554479e99d218dd161
lexieheinle <lexie.heinle@gmail.com>
2017-09-09 16:05:05 -0400

    Working on spreadsheet access

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,3 +1,28 @@
 const Promise = require("bluebird");
 const GoogleSpreadsheet = require("google-spreadsheet");
-const credentials = require('google-credentials.json');
+const credentials = require('./google-credentials.json');
+
+function getSpreadsheet(id) {
+
+    const doc = Promise.promisifyAll(new GoogleSpreadsheet(id));
+
+    doc.useServiceAccountAuthAsync(credentials)
+        .bind(this)
+        .then(function() {
+            return doc.getInfoAsync()
+                .then(function(info) {
+                    return info.worksheets[0];
+                });
+        })
+        .then(function(sheet) {
+            sheet = Promise.promisifyAll(sheet);
+            console.log(sheet);
+
+            return sheet.clearAsync()
+                .then(function() {
+                    return sheet.getRows()
+                });
+        });
+}
+
+getSpreadsheet('1wFXJtXjQ-_JTWUM06RNa0chCdqsm5cF3krhOWMKeQ-Q');
