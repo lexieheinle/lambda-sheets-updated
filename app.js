e8090c8fd3bb247c73d49c5b95055419024554cf
lexieheinle <lexie.heinle@gmail.com>
2017-09-15 16:50:48 -0400

    Add return fix can't find .then()

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const ApiBuilder = require('claudia-api-builder');
 const api = new ApiBuilder();
 const spreadsheet = require('./lib/spreadsheet.js');
 module.exports = api;
-
 api.get('/update', function (request) {
+  console.log(request);
+  console.log(spreadsheet);
   return spreadsheet.getSpreadsheet(request.queryString.id)
   .then(response => {
-    return response;
+    return response
   })
 });
