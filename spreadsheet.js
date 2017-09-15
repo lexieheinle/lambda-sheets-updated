e8d101ba934be456106a6acfafcc3434f4510a13
lexieheinle <lexie.heinle@gmail.com>
2017-09-15 14:08:20 -0400

    Get json upload working

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,6 +1,9 @@
 const Promise = require("bluebird");
 const GoogleSpreadsheet = require("google-spreadsheet");
 const credentials = require('./google-credentials.json');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+const s3 = new AWS.S3();
 
 function getSpreadsheet(id) {
 
@@ -8,21 +11,59 @@ function getSpreadsheet(id) {
 
     doc.useServiceAccountAuthAsync(credentials)
         .bind(this)
-        .then(function() {
+        .then(() => {
             return doc.getInfoAsync()
-                .then(function(info) {
-                    return info.worksheets[0];
+                .then((info) => {
+                  console.log('info stuff')
+                    console.log(info);
+                    let title = info.title.toLowerCase().replace(/ /g,"_");
+                    return [info.worksheets[0], title];
                 });
         })
-        .then(function(sheet) {
-            sheet = Promise.promisifyAll(sheet);
-            console.log(sheet);
+        .then((sheetInfo) => {
+            sheet = Promise.promisifyAll(sheetInfo[0]);
+            const json_spreadsheet = {"updated":new Date(), "id": id, "data": []}
+            const headers = [];
+            return sheet.getCellsAsync({
+                    'min-row': 1,
+                    'max-row': 1
+                })
+                .then((cells) => {
+                    cells.forEach((cell) => {
+                        headers.push(cell.value)
+                    })
+                    return headers;
+                })
+                .then(() => {
+                    return sheet.getRowsAsync()
+                        .then((rows) => {
+                            rows.forEach((row) => {
+                              let rowInfo = {}
+                              headers.forEach((header) => {
+                                rowInfo[header] = row[header]
+                              })
+                              json_spreadsheet['data'].push(rowInfo)
+                            })
+                            //console.log(rows);
+                            return json_spreadsheet
+                        });
+                })
+                .then(() => {
+                  const json_name = sheetInfo[1] + '.json';
+                  const json_string = JSON.stringify(json_spreadsheet);
+                  const uploadPromise = s3.upload({
+                    Bucket: 'sheets-updated', Key: json_name, Body: json_string
+                  }).promise();
 
-            return sheet.clearAsync()
-                .then(function() {
-                    return sheet.getRows()
-                });
+                  return uploadPromise.then((data) => {
+                    console.log('Success');
+                    console.log(data)
+                  }).catch((err) => {
+                    console.log('fail');
+                    console.log(err);
+                  })
+                })
         });
-}
+};
 
 getSpreadsheet('1wFXJtXjQ-_JTWUM06RNa0chCdqsm5cF3krhOWMKeQ-Q');
